e45e347ab7c6a43c616b7df3fbd62c5b82f302a6
dxontop <z4pnu4life@gmail.com>
2026-06-15 13:09:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ var TAB_INTERVAL = 200; /* ★ ms between frames */
 /* ════════════════════════════════════════════════
    ★ MUSIC (plays on splash tap — leave '' to disable)
    ════════════════════════════════════════════════ */
-var MUSIC_SRC = 'https://raw.githubusercontent.com/dxontop/dxontop.github.io/main/2.%20%20SINALOA%20-%20BUDDAHBEADS%2C%20LEXUS%2C%20EJAC%20(YELLOWTAPES%20VOL.1).mp3'; /* ★ path to your .mp3 */
+var MUSIC_SRC = 'https://file.garden/ai-DmyV9Jgejpt9O/Hev%20Abi%20-%20Mossa%20Downtown%20feat.%20gins%26melodies%20(Official%20Music%20Video).mp3'; /* ★ path to your .mp3 */
 var MUSIC_VOL = 0.5;               /* ★ 0.0 – 1.0 */
 
 /* ════════════════════════════════════════════════
