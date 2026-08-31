29ee9eb8d7769aad6c2da12691ce5b9b72509217
dxontop <z4pnu4life@gmail.com>
2026-08-31 13:57:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -216,7 +216,7 @@ const CONFIG = {
 
   mainThreats: [
     { name:'daz', role:'', discordId:'1521890728094208122' },
-    { name:'caliber', music:'pf/caliber.mp3', background: 'pf/caliber.gif', discordId:'1512675755459612835' },
+    { name:'caliber', spotify:'https://open.spotify.com/track/1hoEI997iy6tutEfF5a9M6?si=ef153006a2e64c3a', music:'pf/caliber.mp3', background: 'pf/caliber.gif', discordId:'1512675755459612835' },
     { name:'faiyaz', role:'', discordId:'1402292483584426134' },
   ],
 
